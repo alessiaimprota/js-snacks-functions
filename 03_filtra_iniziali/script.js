a08c6e6bf40799cf10cce44ae50176580e20aa5a
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-09 16:12:39 +0100

    arrow function 3rd

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,8 +1,8 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-const sameLetterNames=[]
-let letter=prompt("Inserisci una lettera").toUpperCase();
+const sameLetterNames = [];
+let letter = prompt("Inserisci una lettera").toUpperCase();
 
 /*
 for(let i=0; i<names.length; i++){
@@ -13,17 +13,24 @@ if (names[i].charAt(0)===letter){
 console.log(sameLetterNames)*/
 
 // Dichiara la funzione qui.
-function nameFilterByFirstChar(oldArray, newArray, sameFirstCharItem){
+/*function nameFilterByFirstChar(oldArray, newArray, sameFirstCharItem){
 for(let i=0; i<oldArray.length; i++){
 if (oldArray[i].charAt(0)===sameFirstCharItem){
     newArray.push(oldArray[i]);
 }
 }
 console.log(newArray)
-}
+}*/
+const nameFilterByFirstChar = (oldArray, newArray, sameFirstCharItem) => {
+  for (let i = 0; i < oldArray.length; i++) {
+    if (oldArray[i].charAt(0) === sameFirstCharItem) {
+      newArray.push(oldArray[i]);
+    }
+  }
+  console.log(newArray);
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-console.log(nameFilterByFirstChar(names, sameLetterNames, letter))
-
+console.log(nameFilterByFirstChar(names, sameLetterNames, letter));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
