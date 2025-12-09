2d4a2a46d1dbb237eafdc8162035be747ea32020
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-09 15:50:00 +0100

    add arrow function

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,22 +1,29 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-const firstLetterNames=[]
+const firstLetterNames = [];
 
 /*seguendo i consigli datomi l'ho risolta prima normalmente 
 for(let i=0; i<names.length; i++){
     firstLetterNames.push(names[i].charAt(0))}
     console.log(firstLetterNames)*/
-    
+
 // Dichiara la funzione qui.
-function firstCharItemArray (originalArray, newArray){
+/*function firstCharItemArray (originalArray, newArray){
     for(let i=0; i<originalArray.length; i++){
     newArray.push(originalArray[i].charAt(0))}
     console.log(newArray)
-}
+}*/
+
+const firstCharItemArray = (originalArray, newArray) => {
+  for (let i = 0; i < originalArray.length; i++) {
+    newArray.push(originalArray[i].charAt(0));
+  }
+  console.log(newArray);
+};
+
 // Invoca la funzione qui e stampa il risultato in console
 
-console.log(firstCharItemArray (names, firstLetterNames));
-
+console.log(firstCharItemArray(names, firstLetterNames));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
