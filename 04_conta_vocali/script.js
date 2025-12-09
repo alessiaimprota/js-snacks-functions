7163b871e351a571ac9d9bf3eed7f6ceadbeace1
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-09 17:01:21 +0100

    4th ex

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,20 +1,31 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-const wordVowel=[]
-const vowel =["a", "e", "i", "o", "u"]
+const vowelsInWord=[]
+const vowels =["a", "e", "i", "o", "u"]
 
-for (let i= 0; i<word.length; i++){
-if(word.charAt(1)===vowel[i]){
-    wordVowel.push(word.charAt(1));
+//il ciclo deve percorrere la parola per la sua interezza per controllare ogni singola lettera. Se la lettera è presente nell'Array vowel dovrà essere pushata in vowelsInWord
+/*for (let i= 0; i<word.length; i++){
+if(vowels.includes(word[i])){
+    vowelsInWord.push(word[i]);
 }
 }
-console.log(wordVowel)
+console.log(vowelsInWord.length)*/
+
 // Dichiara la funzione qui.
+
+function checkVowels (wordToCheck, commonVowels, vowels){
+    for (let i= 0; i<wordToCheck.length; i++){
+        if(vowels.includes(wordToCheck[i])){
+            commonVowels.push(wordToCheck[i]);
+        }
+    }
+    return commonVowels.length;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+const vowelsWordLength = checkVowels (word, vowelsInWord, vowels)
+console.log(vowelsWordLength)
+ 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
